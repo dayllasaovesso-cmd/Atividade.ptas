41cdc31839f1f4e6e040878261b28b7bcaa539b4
Daylla Maria da Silva Lopes Nobrega Sãovesso <daylla.saovesso@estudante.ifms.edu.br>
2026-08-17 15:06:22 -0400

    Content-Type res.end alterado

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const server = http.createServer();
 
 server.on('request', (req, res) => {
     console.log(`Requisição recebida! ${req.method} ${req.url}`);
-    console.log( Data e hora: ${'new Date().toISOString()');
+    console.log('Data e hora: ${'new Date().toISOString()'});
 
     res.statusCode = 201
-    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-    res.end("recurso criado!");    
+    res.setHeader('Content-Type', 'aplication/json');
+    res.end(JSON.stringify({"status": "ok"}));    
 });
 
 server.listen(porta, ()=> {
