f18551fb4e0fa464c5033ae8e397d8cb5efa8cab
Daylla Maria da Silva Lopes Nobrega Sãovesso <daylla.saovesso@estudante.ifms.edu.br>
2026-08-20 17:21:28 +0000

    Servidor criado e rotas adicionadas

--- server.js
+++ server.js
@@ -5,20 +5,36 @@ import { URL } from 'node:url'
 const port = 3000
 
 const status = {
-    "status": "ok",
-    "date": new Date().toISOString()
+"status": "ok",
+"date": new Date().toISOString()
 }
 
-server.on('request', (req, res) => {
-    console.log(`Requisição recebida! ${req.method} ${req.url}`);
-    console.log('data e hora: ${new date().toiSOString()')
+const produtos = [
+{ id: 1, nome: "Sabonete" },
+{ id: 2, nome: "Monitor" },
+{ id: 3, nome: "Cadeira Gamer" }
+]
 
-    res.statusCode = 201
-    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-    //a página trava
 
+const server = http.createServer((req, res) => {
+const urlObj = new URL(req.url, `http://${req.headers.host}`)
+res.statusCode = 200;
+res.setHeader('Content-Type', 'application/json');
+
+if (req.method == "GET" && urlObj.pathname == "/contato") {
+return res.end(JSON.stringify({
+"numero_telefone": "67 99999-9999",
+"endereco": "Rua da Alegria, 99"
+}));
+}
+
+if (req.method == "GET" && urlObj.pathname == "/produtos") {
+return res.end(JSON.stringify(produtos));
+}
+
+res.end(JSON.stringify({ "data": "Página Inicial" }));
 });
 
 server.listen(port, () => {
-    console.log("Servidor funcionando na porta ", port)
+console.log("Servidor funcionando na porta ", port)
 });
